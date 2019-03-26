77cf6c6b823d3fb9c3113bb1f0f19829dae7303f
HevillDS <hevillds@gmail.com>
2019-03-26 14:07:02 +0300

    Started pr2

--- pr2/Program.cs
+++ pr2/Program.cs
@@ -2,6 +2,18 @@
 
 namespace pr2
 {
+    class Band 
+    {
+        public string BandName; //название
+        public string Genre;  //основной жанр
+        public string Country //страна
+
+        public List<Members> Members //участники, поля: лидер, основной состав, для переопределенных + временные участники
+        public List<Instruments> Instruments //основные музыкальные инструменты
+
+
+    }
+
     class Program
     {
         static void Main(string[] args)
