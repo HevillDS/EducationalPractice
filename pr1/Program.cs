3cb3d49ea846f0f1ff015c2255e21efd46136f7a
HevillDS <hevillds@gmail.com>
2019-03-26 13:12:55 +0300

    Added first pr

---
+++ pr1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pract
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Х: ");
+            double x;
+            if (double.TryParse(Console.ReadLine(), out x))
+            {
+             Console.WriteLine("Ввеfghgfghfдите Х: ");   
+                double d = (-Math.Exp((-Math.Cos(Math.Sqrt(x + 5/3)))) - 1.7*Math.Atan( x/5 - 3/4 ) * Math.Sin(1.7 * x));
+                Console.WriteLine(d);
+            }
+            else
+            Console.WriteLine("Введено неверное число!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pr2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
